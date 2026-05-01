53a07a27504a245a38f9a0ee30ed383a1b70f6ae
harshit duhan <harshitduhan2004@gmail.com>
2026-05-01 10:47:04 +0530

    rotation function

---
+++ 396.RotateFunction.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxRotateFunction(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        prev = 0
+        total = sum(nums)
+
+
+        for i in range(n):
+            prev += nums[i] * i
+    
+        res = prev # f(0)
+        for i in range (n - 1, 0 , -1):
+            prev = prev + (total - nums[i]) - (n - 1)* nums[i]
+            res = max(res ,prev)
+
+        return res
